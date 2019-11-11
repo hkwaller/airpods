61353b2f3c79a11de542ce069e03284a8146ba33
Hannes Waller <hkwaller@gmail.com>
2019-11-11 14:12:21 +0100

    Initial Commit

---
+++ AirPods/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AirPods
+//
+//  Created by Hannes Waller on 2019-11-11.
+//  Copyright © 2019 Hannes Waller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AirPods/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AirPods
+//
+//  Created by Hannes Waller on 2019-11-11.
+//  Copyright © 2019 Hannes Waller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
