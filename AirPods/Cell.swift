36e46bb5a5d8454607cab77a7759a84c48a27760
Hannes Waller <hkwaller@gmail.com>
2019-11-12 16:27:30 +0100

    adding gitignore

---
+++ AirPods/Cell.swift
@@ -0,0 +1,13 @@
+//
+//  Cell.swift
+//  AirPods
+//
+//  Created by Hannes Waller on 2019-11-12.
+//  Copyright © 2019 Hannes Waller. All rights reserved.
+//
+
+import Cocoa
+
+class Cell: NSCell {
+
+}
---
+++ AirPods/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AirPods
+//
+//  Created by Hannes Waller on 2019-11-12.
+//  Copyright © 2019 Hannes Waller. All rights reserved.
+//
+
+import Cocoa
+
+class Item: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
